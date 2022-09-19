e9b5bffdeb498f7c680fd1bd6b5f0e06f7eaba25
sunny-117 <zhiqiangfu6@gmail.com>
2022-09-19 21:36:01 +0800

    feat: list

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,16 @@
+import { useState } from "react"
 import RenderArrayDataIntoListsAndTables from "./package/RenderArrayDataIntoListsAndTables"
 
 
 const App = () => {
+  const [loading, setLoading] = useState(false)
   return (
     <div>
-      <RenderArrayDataIntoListsAndTables data={[{ name: 'John', surname: 'Smith', age: 42 },
+      {/* <RenderArrayDataIntoListsAndTables data={[{ name: 'John', surname: 'Smith', age: 42 },
       { name: 'Adam', surname: 'Smith', gender: 'male' },]} propertyNames={[
         'a', 'b'
-      ]} />
-    </div>
+      ]} /> */}
+    </div >
   )
 }
 
