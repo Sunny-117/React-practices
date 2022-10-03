30ce908eff7963b4de508ade3f91302f112a97cd
sunny-117 <zhiqiangfu6@gmail.com>
2022-10-03 23:01:10 +0800

    kkhj

---
+++ my-app/src/package/Reverse.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+
+
+
+// 传入一个组件列表
+// 返回反转之后的Comp
+export const Reverse = ({ ListComp, arr }) => {
+
+    return (
+        <div>
+            <ListComp />
+        </div>
+    )
+}
