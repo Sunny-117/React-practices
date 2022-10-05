aa1756e707eb3e5de2e7145861833298ea6b4f53
sunny-117 <zhiqiangfu6@gmail.com>
2022-10-05 18:50:46 +0800

    feat

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,22 +1,28 @@
-import { useState } from "react"
+import React, { useState } from 'react';
+import { Reverse } from './package/Reverse';
 
-import RenderArrayDataIntoListsAndTables from "./package/RenderArrayDataIntoListsAndTables"
-
-
-const App = () => {
-  const [loading, setLoading] = useState(false)
-  return (
-    <div>
-      {/* <RenderArrayDataIntoListsAndTables data={[{ name: 'John', surname: 'Smith', age: 42 },
-      { name: 'Adam', surname: 'Smith', gender: 'male' },]} propertyNames={[
-        'a', 'b'
-      ]} /> */}
-
-      <button>
-
-      </button>
-    </div >
-  )
+const ListComp = () => {
+  const [arr, setArr] = useState([1, 2, 3, 4, 5]);
+  const handleClick = () => {
+    const newArr = arr.reverse();
+    setArr(newArr.slice())
+  }
+  return <div>
+    <ul>
+      {arr.map(item => {
+        return <li>{item}</li>
+      })}
+    </ul>
+    <button onClick={handleClick}>dianji </button>
+  </div>
 }
 
-export default App
+
+export default function App() {
+  return (
+    <div>
+      {/* @ts-ignore */}
+      <Reverse ListComp={ListComp} />
+    </div>
+  )
+}
