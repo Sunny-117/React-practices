313b52d5b8cfce0125b7060a74c861bac2173fa0
付志强 <zhiqiangfu6@gmail.com>
2022-11-01 20:43:40 +0800

    feat: add new feature

--- src/components/Visual视觉效果渲染/Collapse/index.tsx
+++ src/components/Visual视觉效果渲染/Collapse/index.tsx
@@ -1,38 +1,21 @@
 import { useState } from "react";
 
-export function Collapse(props) {
-    console.log(props.collapsed)
-    const [isCollapsed, setIsCollapsed] = useState(props.collapsed);
+export function Collapse({ collapsed, children }) {
 
-    const style = {
-        collapsed: {
-            display: "none"
-        },
-        expanded: {
-            display: "block"
-        },
-        buttonStyle: {
-            display: "block",
-            width: "100%"
-        }
-    };
+    const [isCollapsed, setIsCollapsed] = useState(collapsed);
+
 
     return (
         <div>
             <button
-                style={style.buttonStyle}
                 onClick={() => setIsCollapsed(!isCollapsed)}
             >
                 {isCollapsed ? "显示" : "隐藏"} 内容
             </button>
             <div
-                className="collapse-content"
-                // 决定显示和折叠
-                style={isCollapsed ? style.collapsed : style.expanded}
-                // aria-expanded 是给 Screen Reader 用来 判断当前元素状态的辅助属性
-                aria-expanded={isCollapsed}
+
             >
-                {props.children}
+                {children}
             </div>
         </div>
     );
