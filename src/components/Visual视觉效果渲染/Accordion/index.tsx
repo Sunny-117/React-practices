4ed49697eeb42f478292b23d0bf7b4822b9baaa6
付志强 <zhiqiangfu6@gmail.com>
2022-11-01 19:39:30 +0800

    feat: add new feature

--- src/components/Visual视觉效果渲染/Accordion/index.tsx
+++ src/components/Visual视觉效果渲染/Accordion/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function AccordionItem() {
 
 }
-export default function Accordion() {
+export default function Accordion({ defaultIndex, onItemClick }) {
     return (
         <div>index</div>
     )
