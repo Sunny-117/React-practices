86e15bf3389d7cef8b615476541554e755907beb
付志强 <zhiqiangfu6@gmail.com>
2022-11-01 20:05:20 +0800

    feat: add new feature

--- src/components/Visual视觉效果渲染/Accordion/index.tsx
+++ src/components/Visual视觉效果渲染/Accordion/index.tsx
@@ -1,10 +1,14 @@
 export function AccordionItem(props) {
 
     return <div className="item">
+        <button>{props.label}</button>
         {props.children}
     </div>
 }
 
 export function Accordion(props) {
 
+    return <div>
+        {props.children}
+    </div>
 }
