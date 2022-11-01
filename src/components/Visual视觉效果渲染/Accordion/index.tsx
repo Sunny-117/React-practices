dbdd5ae5cef47cf299d68d47bda4b7d92a34cd77
付志强 <zhiqiangfu6@gmail.com>
2022-11-01 20:07:50 +0800

    feat: add new feature

--- src/components/Visual视觉效果渲染/Accordion/index.tsx
+++ src/components/Visual视觉效果渲染/Accordion/index.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react"
+
 export function AccordionItem(props) {
+    const [i, setI] = useState(props.defaultIndex)
     return <div className="item">
         <button>{props.label}</button>
-        <div >
+        <div style={{
+            display: props.index === i ? 'block' : 'none'
+        }}>
             {props.children}
         </div>
     </div>
