24998a2a7d7169063579e074f2463f527d882e72
付志强 <zhiqiangfu6@gmail.com>
2022-11-01 19:27:50 +0800

    feat: add new feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ export const App = () => {
   return (
 
     <div>
+      <Accordion defaultIndex="1" onItemClick={console.log}>
+        <AccordionItem label="A" index="1">
+          Lorem ipsum
+        </AccordionItem>
+        <AccordionItem label="B" index="2">
+          Dolor sit amet
+        </AccordionItem>
+      </Accordion>
     </div>
   )
 }
