2e03cf51c85e7d6d04c651b9970072ea0c46e83f
付志强 <zhiqiangfu6@gmail.com>
2022-11-01 19:59:30 +0800

    feat: add new feature

--- src/components/Visual视觉效果渲染/Accordion/index.tsx
+++ src/components/Visual视觉效果渲染/Accordion/index.tsx
@@ -1,7 +1,11 @@
 
 import React from "react";
-export function AccordionItem(props) {
-
+export function AccordionItem({ label, index, isCollapsed, children }}) {
+    return <div>
+        <p>
+            <span>{label}</span>
+        </p>
+    </div>
 }
 
 export function Accordion(props) {
