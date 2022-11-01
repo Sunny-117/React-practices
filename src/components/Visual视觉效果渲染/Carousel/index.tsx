3354b33b5d5722dc7c6aafbe2feb795a5985934e
付志强 <zhiqiangfu6@gmail.com>
2022-11-01 20:34:30 +0800

    feat: add new feature

--- src/components/Visual视觉效果渲染/Carousel/index.tsx
+++ src/components/Visual视觉效果渲染/Carousel/index.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 
-export function Carousel({ carouselItems }) {
+export function Carousel(props) {
     let [i, setI] = useState(0)
     useEffect(() => {
         setTimeout(() => {
+            const { carouselItems } = props;
             setI((i + 1) % carouselItems.length)
         }, 500)
     }, [])
