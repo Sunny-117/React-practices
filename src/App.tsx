b4e149462b43413c2e2dcc698fe965d7b8f2849f
付志强 <zhiqiangfu6@gmail.com>
2022-11-07 22:05:05 +0800

    feat: add new feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 // 实现一个 百度搜索框 （搜索提示）
 
 export default function App() {
+  const [value, setValue] = useState('')
+  const [show, setShow] = useState(false)
   const list = [
     '前端1', '前端2', '前端3', '前端3', '前端5', '前端5', '前端6', '前端7',
   ]
   return (
-    <div>App</div>
+    <div>
+      <input type="text" name="" id=""
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value)
+        }}
+      />
+      <ul>
+        {list.map((item, index) => {
+          return <li key={index}>
+            {item}
+          </li>
+        })}
+      </ul>
+    </div>
   )
 }
