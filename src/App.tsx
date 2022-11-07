d4041998df0492a1b4a07b76eaa8db4e04a45460
付志强 <zhiqiangfu6@gmail.com>
2022-11-07 22:04:15 +0800

    feat: add new feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 // 实现一个 百度搜索框 （搜索提示）
 
-
-// const list = [
-//   '前端1', '前端2', '前端3', '前端3', '前端5', '前端5', '前端6', '前端7',
-// ]
-
 export default function App() {
+  const list = [
+    '前端1', '前端2', '前端3', '前端3', '前端5', '前端5', '前端6', '前端7',
+  ]
   return (
     <div>App</div>
   )
