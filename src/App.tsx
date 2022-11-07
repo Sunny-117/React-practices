8856029c4eb550273171213e74ed8b070a483fbe
付志强 <zhiqiangfu6@gmail.com>
2022-11-07 21:45:05 +0800

    feat: add new feature

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { Mailto } from "./components/Visual视觉效果渲染/Mailto"
 import { Modal } from "./components/Visual视觉效果渲染/Modal"
 import { StarRating } from "./components/Visual视觉效果渲染/StarRating"
 import { TabItem, Tabs } from "./components/Visual视觉效果渲染/Tabs"
-import Ticker from "./components/Visual视觉效果渲染/Ticker"
+// import Ticker from "./components/Visual视觉效果渲染/Ticker"
 // import { Ticker } from "./components/Visual视觉效果渲染/Ticker"
 
 
@@ -143,7 +143,7 @@ export const App = () => {
       <div className="ticker" style={{
         margin: '200px'
       }}>
-        <Ticker times={5} interval={1000} />
+        {/* <Ticker times={5} interval={1000} /> */}
       </div>
     </div>
   )
