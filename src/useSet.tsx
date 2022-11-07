4488ded6dcbd9674a0f9155dcd44e7272ed94aff
付志强 <zhiqiangfu6@gmail.com>
2022-11-07 21:49:15 +0800

    feat: add new feature

--- src/useSet.tsx
+++ src/useSet.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 export default function useSet(interator) {
-    return (
-        <div>
+    const set = new Set(interator)
 
-        </div>
-    )
+    const methods = {
+        add(item: T): void;
+        addAll(items: Iterable<T>): void;
+        delete(item: T): void;
+        deleteAll(items: Iterable<T>): void;
+        clear(): void;
+    }
+
+    return [
+        set,
+        methods
+    ]
 }
