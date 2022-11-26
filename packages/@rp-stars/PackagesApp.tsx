8d68cd194dc441430b52944ad5eb991909904511
付志强 <zhiqiangfu6@gmail.com>
2022-11-26 18:29:01 +0800

    feat: add new feature

---
+++ packages/@rp-stars/PackagesApp.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Test from './Autocomplete/test'
+
+function PackagesApp() {
+    return (
+        <div>
+            <Test />
+        </div>
+    )
+}
+
+export default PackagesApp
