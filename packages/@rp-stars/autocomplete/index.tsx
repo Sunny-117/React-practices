e1e99cc85e270e0800852d2a067a6c494a87396c
付志强 <zhiqiangfu6@gmail.com>
2022-11-26 18:31:31 +0800

    feat: add new feature

--- packages/@rp-stars/autocomplete/index.tsx
+++ packages/@rp-stars/autocomplete/index.tsx
@@ -1,25 +1,17 @@
 import React, { useEffect, useState } from 'react'
-
-import axios from 'axios';
-
-
-
+import axios from 'axios'
 function Autocomplete() {
     const [data, setData] = useState({ hits: [] });
     const [query, setQuery] = useState('react');
-
     useEffect(() => {
         let ignore = false;
-
         async function fetchData() {
             const result = await axios('https://hn.algolia.com/api/v1/search?query=' + query);
             if (!ignore) setData(result.data);
         }
-
         fetchData();
         return () => { ignore = true; }
     }, [query]);
-
     return (
         <>
             <input value={query} onChange={e => setQuery(e.target.value)} />
